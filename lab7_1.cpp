03f5959897205c323efa025f783198aa8a65e5eb
lalinnx <lalinpye@gmail.com>
2021-01-01 23:29:49 +0700

    finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -6,12 +6,16 @@ int main()
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
+  if (rank == 'S')
+    cout << "You have received Super Ultra Rare Unit!!!\n";
+  if (rank == 'S' or rank == 'A') 
+    cout << "You have received 5 gems.\n";
+  if (rank == 'S' or rank == 'A' or rank == 'B') 
+    cout << "You have received 1 gems.\n";
+  if (rank == 'S' or rank == 'A' or rank == 'B' or rank == 'C') 
+    cout << "You have received 2000 coins.\n";
+  if (rank == 'S' or rank == 'A' or rank == 'B' or rank == 'C' or rank == 'D') 
+    cout << "You have received very KAK items.\n";
+  
   return 0;
 }
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a,h,p;
+    string s;
+
+    cout << "Enter your age: ";
+    cin >> a;
+
+    if(a<=20){
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<160){
+            s="UNFRIEND";
+        }else if(h<175){
+            s="FRIEND";
+        }else{
+            cout << "Enter your property: ";
+            cin >> p;
+            if(p>69000000){
+                s="MARRIED";
+            }else{
+                s="ONE-NIGHT-STAND"; 
+            }
+        }
+    }else if(a<30){
+        cout << "Enter your property: ";
+            cin >> p;
+            if(p>10000000){
+                s="BEST FRIEND";
+            }else{
+                s="UNFRIEND"; 
+            }
+    }else{
+        s="UNFRIEND";
+    }
+    cout << "Your status = " << s;
+    
+  
+return 0;
+}
